4406d12c152516af052e6fa7bf704cc500341d66
chenmingyong0423 <474571928@qq.com>
2024-02-06 20:25:12 +0800

    - 新增贡献指南文档 - 目录大纲显示级别调整为 [1, 6]

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -1,26 +1,31 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress'
 
-export const en =  defineConfig({
+export const en = defineConfig({
     title: "go-mongox",
     description: "The documentation for go-mongox",
     themeConfig: {
         nav: [
-            { text: 'Guide', link: '/en/introduction/getting-started' },
+            {text: 'Guide', link: '/en/introduction/getting-started'},
+            {text: 'Contribute', link: '/en/contribute'},
         ],
+
+        outline: {
+            level: [1, 6]
+        },
 
         sidebar: [
             {
                 text: 'Introduction',
                 collapsed: true,
                 items: [
-                    { text: 'What is go-mongox?', link: '/en/introduction/what-is-go-mongox' },
-                    { text: 'Getting Started', link: '/en/introduction/getting-started' }
+                    {text: 'What is go-mongox?', link: '/en/introduction/what-is-go-mongox'},
+                    {text: 'Getting Started', link: '/en/introduction/getting-started'}
                 ]
             }
         ],
 
         socialLinks: [
-            { icon: 'github', link: 'https://github.com/chenmingyong0423/go-mongox-doc' }
+            { icon: 'github', link: 'https://github.com/chenmingyong0423/go-mongox' }
         ],
         footer: {
             message: 'Documentation licensed under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0.</a>',
--- docs/.vitepress/config/zh.ts
+++ docs/.vitepress/config/zh.ts
@@ -1,27 +1,35 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress'
 
-export const zh =  defineConfig({
+export const zh = defineConfig({
     title: "go-mongox",
     description: "The documentation for go-mongox",
     themeConfig: {
         nav: [
-            { text: '指南', link: '/introduction/getting-started' },
+            {text: '指南', link: '/introduction/getting-started'},
+            {text: '贡献', link: '/contribute'},
         ],
 
+        outline: {
+            label: '页面导航',
+            level: [1, 6]
+        },
+
         sidebar: [
+
             {
                 text: '简介',
                 collapsed: true,
                 items: [
-                    { text: '什么是 go-mongox？', link: '/introduction/what-is-go-mongox' },
-                    { text: '快速开始', link: '/introduction/getting-started' }
+                    {text: '什么是 go-mongox？', link: '/introduction/what-is-go-mongox'},
+                    {text: '快速开始', link: '/introduction/getting-started'}
                 ]
             }
         ],
 
         socialLinks: [
-            { icon: 'github', link: 'https://github.com/chenmingyong0423/go-mongox-doc' }
+            { icon: 'github', link: 'https://github.com/chenmingyong0423/go-mongox' }
         ],
+
         footer: {
             message: '文档根据 <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC BY 4.0.</a> 许可证授权',
             copyright: `版权所有 © 2024-${new Date().getFullYear()} <a href="https://github.com/chenmingyong0423" target="_blank">陈明勇</a>`
