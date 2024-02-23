ae8918bce663f83b51a7c2504bba8eb466ce4852
陈明勇 <chenmingyong5873@126.com>
2024-02-23 11:41:18 +0800

    添加百度站点验证配置

--- docs/.vitepress/config/shared.ts
+++ docs/.vitepress/config/shared.ts
@@ -9,6 +9,7 @@ export const shared = defineConfig({
     head: [
         ['link', { rel: 'icon', type: 'image/png', href: '/go-mongox-logo.png' }],
         [ 'meta', { name: 'google-site-verification', content: 'e5RBACChMwOXmopO_t2NOOv6GWbHv5NdQw-eCxILdtc' } ],
+        [ 'meta', { name: 'baidu-site-verification', content: 'codeva-sIm3HwlyYX' } ],
         ['meta', { property: 'og:type', content: 'website' }],
         ['meta', { property: 'og:locale', content: 'zh' }],
         ['meta', { property: 'og:title', content: 'go-mongox 指南' }],
